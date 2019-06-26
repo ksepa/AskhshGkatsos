2c221e36d5700ac0b79abef1656125952a9bf485
Ksepa <stavros_x@hotmail.com>
2019-06-26 21:59:00 +0300

    UnitTest

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -11,4 +11,60 @@ namespace UnitTestProject
         {
         }
     }
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+    }
+    [TestClass]
+    public class UnitTest3
+    {
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+    }
+    [TestClass]
+    public class UnitTest4
+    {
+        [TestMethod]
+        public void TestMethod4()
+        {
+        }
+    }
+    [TestClass]
+    public class UnitTest5
+    {
+        [TestMethod]
+        public void TestMethod5()
+        {
+        }
+    }
+    [TestClass]
+    public class UnitTest6
+    {
+        [TestMethod]
+        public void TestMethod6()
+        {
+        }
+    }
+    [TestClass]
+    public class UnitTest7
+    {
+        [TestMethod]
+        public void TestMethod7()
+        {
+        }
+    }
+    [TestClass]
+    public class UnitTest8
+    {
+        [TestMethod]
+        public void TestMethod8()
+        {
+        }
+    }
 }
